029933b78453f4fc317e3172230bc854eb91c71f
caclm10 <lewinxander@gmail.com>
2024-06-21 21:42:36 +0700

    update font family configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,33 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Figtree } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const figtree = Figtree({
+    subsets: ["latin"],
+    variable: "--font-figtree",
+});
 
 export const metadata: Metadata = {
-  title: "Counter",
-  description: "Just a simple counter.",
+    title: "Counter",
+    description: "Just a simple counter.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={cn(
+                    figtree.variable,
+                    "font-figtree antialiased bg-background text-foreground"
+                )}
+            >
+                <div className="min-h-screen">{children}</div>
+            </body>
+        </html>
+    );
 }
