fe6c2c6f37334a1f9179db8a84e303c511ceca61
caclm10 <lewinxander@gmail.com>
2024-06-23 20:39:19 +0700

    remove unsued import

--- app/create/form.tsx
+++ app/create/form.tsx
@@ -10,7 +10,6 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
